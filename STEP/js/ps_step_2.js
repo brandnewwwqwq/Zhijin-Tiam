ca797e866d34a09260d6f0cdacc8504c539a79d8
brandnewwwqwq <zhijin.tian@hfg.design>
2022-02-15 15:53:50 +0100

    Hausaufgabe 2.1
    1. Click: Konfetti wird größer

--- STEP/js/ps_step_2.js
+++ STEP/js/ps_step_2.js
@@ -1,13 +1,22 @@
 Homeworks.aufgabe = 2;
+let click = 1;
+let ellipseGross = 20;
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('thecanvas');
+  document.addEventListener('click', onMouseClick);
 }
 
 function draw() {
-  background(40, 10);
+  background(50, 10);
   let c = color(Math.random() * 256, Math.random() * 256, Math.random() * 256);
   fill(c);
-  ellipse(Math.random() * windowWidth, Math.random() * windowHeight, 20, 20);
+  ellipse(Math.random() * windowWidth, Math.random() * windowHeight, ellipseGross, ellipseGross);
+}
+
+function onMouseClick() {
+  if (click = 1){
+  ellipseGross = 40;
+}
 }
