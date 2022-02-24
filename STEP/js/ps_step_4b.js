1cd6b68593ae155af30023c8a40032abd98a5da6
brandnewwwqwq <zhijin.tian@hfg.design>
2022-02-24 09:45:01 +0100

    HA 4b
     Spielfeld mit 'smarten' Feldern

--- STEP/js/ps_step_4b.js
+++ STEP/js/ps_step_4b.js
@@ -1,63 +1,126 @@
 Homeworks.aufgabe = 4;
 
-let fields = [];
+Homeworks.aufgabe = 4;
+/*1. Auf Click ändert ein zufälliges Field die Farbe
+2. auf Click ändert ein Field die Form
+3. auf Click ändert ein zufälliges Field die Grösse  (optional)
+4. oder was Euch sonst so interessiert (optional)
+*/
 
-//let hugoObject = {color: 'green', pos: {x: 90, y: 0}, size: 20};
+let colors = [];
+let size = 40;
+let gap = 8;
+let numCol = 10;
+let numRow = 10;
+let borderX;
+let borderY;
+let bereitgesamt = numCol * size + (numCol - 1) * gap;
+let hochgesamt = numRow * size + (numRow - 1) * gap;
+//zufällige Größe
+let zufallsSize
+//zufällige position
+let zufallsX;
+let zufallsY;
+let feld = [];
+//zufällige colors
+let r;
+let g;
+let b;
+//zufällige form
+let useRect = true;
+//原来有的（class是个特别的function）
+//必须用constructor
+
 
 class Block {
-
   constructor(attrs) {
     this.attrs = attrs;
   }
-
   draw() {
     fill(this.attrs.color);
+    rectMode(CENTER);
     if (this.attrs.useRect) {
-      rect(this.attrs.pos.x, this.attrs.pos.y, this.attrs.size, this.attrs.size);
+      rect(
+        this.attrs.pos.x,
+        this.attrs.pos.y,
+        this.attrs.size,
+        this.attrs.size);
     } else {
-      ellipse(this.attrs.pos.x, this.attrs.pos.y, this.attrs.size, this.attrs.size);
+      ellipse(
+        this.attrs.pos.x,
+        this.attrs.pos.y,
+        this.attrs.size,
+        this.attrs.size);
     }
   }
-
-  // sleep() {
-  //   console.log(this.attrs);
-  // }
-
-  changeColor(newColor) {
-
-  }
-
 }
 
-let size = 20;
-let gap = 8;
-let numCol = 15;
-let numRow = 17;
 
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent('thecanvas');
-
-  let patternWidth = (size + gap) * numCol - gap;
-  let borderX = (windowWidth - patternWidth) / 2;
-  let patternHeight = (size + gap) * numRow - gap;
-  let borderY = (windowHeight - patternHeight) / 2;
-
-  for (let col = 0; col < numCol; col += 1) {
-    fields[col] = [];
+  document.addEventListener('click', onMouseClick);
+  borderY = (windowHeight - hochgesamt) / 2;
+  borderX = (windowHeight - bereitgesamt) / 2;
+  for (let col = 0; col < numCol; col++) {
+    feld[col] = [];
     for (let row = 0; row < numRow; row++) {
-      fields[col][row] = new Block({color: 'red', pos: {x: borderX + col * (size + gap), y: borderY + row * (size + gap)}, size: size, useRect: true});
+      feld[col][row] = new Block({
+        color: 'white',
+        pos: {
+          x: borderX + (size + gap) * col,
+          y: borderY + (size + gap) * row
+        },
+        size: size,
+        useRect: true
+      }); //block ({...});
     }
   }
 }
 
-function draw() {
-  for (let col = 0; col < numCol; col += 1) {
-    for (let row = 0; row < numRow; row++) {
-      fields[col][row].draw();
-    }
+
+function onMouseClick() {
+  //zufällige position
+  zufallsX = round(random(0, numCol - 1));
+  zufallsY = round(random(0, numRow - 1));
+  // feld[0] = zufallsX;
+  // feld[1] = zufallsY;
+  console.log("zufallsposition", zufallsX, zufallsY);
+  //zufällige Größe
+  zufallsSize = Math.floor(random(size, size * 2));
+  console.log("size", zufallsSize);
+  feld[zufallsX][zufallsY].attrs.size = zufallsSize;
+  //zufällige colors
+  r = Math.floor(Math.random() * (255 - 0 + 1));
+  g = Math.floor(Math.random() * (255 - 0 + 1));
+  b = Math.floor(Math.random() * (255 - 0 + 1));
+  console.log("color", r, g, b);
+  feld[zufallsX][zufallsY].attrs.color = color(r, g, b);
+  //zufällige form
+  var zufall = random(0, 1);
+  if (zufall < 0.5) {
+    feld[zufallsX][zufallsY].attrs.useRect = true;
+  } else {
+    feld[zufallsX][zufallsY].attrs.useRect = false;
   }
-  // fields.forEach((field, idx) => {
-  //   field.draw();
-  // });
+  console.log("zufall", zufall);
+} //function
+
+
+function draw() {
+  for (let col = 0; col < numCol; col++) {
+    for (let row = 0; row < numRow; row++) {
+      feld[col][row].draw(); //warum gibt es【】？
+    } //for
+  } //for
+} //function
+
+
+function keyTyped() {
+  let divToHide = document.getElementsByClassName("overlay")[0];
+  if (key == "h") {
+    divToHide.style.visibility = "hidden";
+  } else if (key == "s") {
+    divToHide.style.visibility = "visible"
+  }
 }
